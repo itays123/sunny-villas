c5e96396b04742f631969dc48239d35eabee4d70
itays123 <itays2005@gmail.com>
2020-08-08 10:03:16 +0300

    update to attain 1.0.10 (#1)

--- serverDeps.ts
+++ serverDeps.ts
@@ -7,7 +7,7 @@ export {
   ViewEngine,
   Request,
   Response,
-} from 'https://deno.land/x/attain@1.0.6/mod.ts';
+} from 'https://deno.land/x/attain@1.0.10/mod.ts';
 export { validateJwt } from 'https://deno.land/x/djwt/validate.ts';
 export { makeJwt, Jose, JwtInput } from 'https://deno.land/x/djwt/create.ts';
 export { Client } from 'https://deno.land/x/postgres/mod.ts';
--- view/deps.tsx
+++ view/deps.tsx
@@ -1,10 +1,10 @@
-// @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
+// @deno-types="https://raw.githubusercontent.com/Soremwar/deno_types/master/react/v16.13.1/react.d.ts"
 import React from "https://jspm.dev/react@16.13.1";
 
-// @deno-types="https://deno.land/x/types/react-dom/v16.13.1/server.d.ts"
+// @deno-types="https://raw.githubusercontent.com/Soremwar/deno_types/master/react-dom/v16.13.1/server.d.ts"
 import ReactDOMServer from "https://jspm.dev/react-dom@16.13.1/server";
 
-// @deno-types="https://deno.land/x/types/react-dom/v16.13.1/react-dom.d.ts"
+// @deno-types="https://raw.githubusercontent.com/Soremwar/deno_types/master/react-dom/v16.13.1/react-dom.d.ts"
 import ReactDOM from "https://jspm.dev/react-dom@16.13.1";
 
 import MatUI from "https://jspm.dev/@material-ui/core@4.11.0";
@@ -14,13 +14,13 @@ import ForwardIconComponent from "https://jspm.dev/@material-ui/icons@4.9.1/Forw
 const MenuIcon = MenuIconComponent.default;
 const ForwardIcon = ForwardIconComponent.default;
 
-import { useRouter } from "https://deno.land/x/attain@1.0.6/react/AttainRouter.js";
+import { useRouter } from "https://deno.land/x/attain@1.0.10/react/AttainRouter.js";
 import {
   useDocument,
   addMeta,
   addScript,
   setTitle,
-} from "https://deno.land/x/attain@1.0.6/react/AttainReactUtils.js";
+} from "https://deno.land/x/attain@1.0.10/react/AttainReactUtils.js";
 
 export {
   React,
